73d3b0af19d1fe71dfc6af7ad0e35c70b0426a3e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-21 12:34:29 -0400

    playing around with antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <h1>Know My World Mockup</h1>
+      <Button type="primary">Hello There</Button>
     </div>
   );
 }
